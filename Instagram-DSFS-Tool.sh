2ea2e0e5eeb7aae4273a5e4b7555642ef60bb903
Anon6372098 <43740735+Anon6372098@users.noreply.github.com>
2018-11-03 21:57:27 +0700

    Create Instagram-DSFS-Tool.sh

---
+++ Instagram-DSFS-Tool.sh
@@ -0,0 +1,70 @@
+#!/system/xbin/bash
+clear
+blue='\033[34;1m'
+green='\033[32;1m'                                        
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'                                           
+yellow='\033[33;1m'
+sleep 1
+toilet -f slant -F gay "Instagram" | lolcat
+toilet -f slant -F gay "DSFS Tool" | lolcat
+echo "\033[35;1m"
+cal
+date
+echo ""      
+echo "\033[32;1mCreator : Anon6372098" 
+echo "\033[35;1mTeam : D4RK SYST3M F41LUR3 S33K3R (DSFS)"
+echo "\033[36;1mThanks to : Tuan c4rt00nw4r"
+echo "\033[37;1mAlly Team : DSFS Public"
+echo "\033[33;1mContact Me : anon6372098@gmail.com"
+echo "\033[31;1mSubscribe Youtube : D4RK SYST3M F41LUR3 S33K3R"
+echo "\033[34;1m[============================]"
+sleep 1
+echo ""
+echo "\033[32;1mSilahkan Pilih Tool Intagram :"
+echo "\033[36;1m"
+echo "[===============================================]"
+echo "1.> InstaBrute DSFS Anon (Hack Instagram)"
+echo "[===============================================]"
+echo "2.> InstaBot (Belum Tersedia, Next Rilis v2.0)"
+echo "[===============================================]"
+echo "0.> Keluar/Exit"
+echo "[===============================================]"
+read -p "root@InstagramDSFSTool]~#" anon
+
+if [ $anon = 1 ] || [ $anon = 1 ]
+then
+clear
+figlet "InstaBrute DSFS" | lolcat
+python2 InstaBrute-DSFS-Anon.py
+fi
+
+if
+[ $anon = 2 ] || [ $anon = 2 ]
+then
+clear
+figlet "Belum Tersedia" | lolcat
+fi
+
+if [ $anon = 0 ] || [ $anon = 00 ]
+then
+echo "\033[32;1mWe Are D4RK SYST3M F41LUR3 S33K3R (DSFS)"
+sleep 1
+echo "\033[32;1mWe Are DSFS Public"
+sleep 1
+echo "\033[33;1mWe Are Anonymous"
+sleep 1
+echo "We Are Legion"
+sleep 1
+echo "We Do Not Forgive"
+sleep 1
+echo "We Do Not Forget"
+sleep 1
+echo "Expect Us"
+sleep 1
+echo "\033[32;1mDiakui, Bukan Mengakui"
+sleep 1
+exit
+fi
